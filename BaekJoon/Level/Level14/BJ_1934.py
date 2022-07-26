857006cb421beec227e1b0e676117361d96c234f
kjw <wodn3322@gmail.com>
2022-07-27 04:31:33 +0900

    20220727-04:31 solved BJ 1943

--- BaekJoon/Level/Level14/BJ_1934.py
+++ BaekJoon/Level/Level14/BJ_1934.py
@@ -1,6 +1,29 @@
 import sys
 
+
 def solution():
+    caseNum = int(sys.stdin.readline().strip())
+    caseList = [[]] * caseNum
+    for i in range(caseNum):
+        caseList[i] = list(map(int, sys.stdin.readline().strip().split(' ')))
+
+    def uclid(a, b):
+        remain = max(a, b) % min(a, b)
+        if remain == 0:
+            return min(a, b)
+        if min(a, b) % remain != 0:
+            return uclid(remain, min(a, b))
+        else:
+            return remain
+
+    answerList = [0] * caseNum
+    for i in range(caseNum):
+        greatestCommon = uclid(caseList[i][0], caseList[i][1])
+        answerList[i] = int(caseList[i][0] * caseList[i][1] / int(greatestCommon))
+
+    for i in answerList:
+        sys.stdout.write(str(i) + '\n')
+
     return
 
 
